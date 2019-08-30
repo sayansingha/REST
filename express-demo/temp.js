a06b5fd053f55d3d0a1f40d1ec36097ec0ffe31f
Sayan Singha <sayansingha@Sayans-MacBook-Pro.local>
2019-08-30 18:50:19 +0530

    It has the basic making of api via express

---
+++ express-demo/temp.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+const weather = [
+    {place: "raiganj", summary: "windy and warm"},
+    {place: "kolkata", summary: "windy and humid"},
+    {place: "maldah", summary: "windy and rainy"}
+]
+
+// app.get('/',(req,res)=>{
+//     res.send('Hello World!!')
+// })
+// app.get('/api/secret',(req,res)=>{
+//     res.send({
+//         name : "James Bond",
+//         ID: "007"
+//     })
+// })
+// app.get('/api/posts/:year/:month', (req,res)=>{
+//     res.send(req.params)
+// })
+app.get('/api/geocode/',(req,res)=>{
+    res.send(weather)
+})
+
+app.get('/api/geocode/:place', (req,res)=>{
+   const i = weather.find((c)=> c.place === req.params.place)
+   if(!i){
+       res.status(404).send("NOT FOUND!")
+   }
+   else{
+       res.send(i)
+   }
+})
+
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`) //it wll show 5000 because I have put "export PORT=5000"
+})
+
+// app.post()
+// app.put()
+// app.delete()
+
+
